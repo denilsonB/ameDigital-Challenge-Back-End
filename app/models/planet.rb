526111f56dd5b9e2e5f2fe07dd47fca202dcf892
Denilson Bulhões <denilson1569@hotmail.com>
2023-11-16 23:28:05 -0300

    create planets index

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,2 +1,13 @@
 class Planet < ApplicationRecord
+  include HTTParty
+
+  before_create :set_number_of_appearances
+
+  def set_number_of_appearances
+    api_request = self.class.get("https://swapi.dev/api/planets/?search=#{self.name.downcase}")
+    
+    return 0 if api_request["results"].count == 0
+
+    self.number_of_appearances = api_request["results"][0]["films"].try(:count)
+  end
 end
--- spec/models/planet_spec.rb
+++ spec/models/planet_spec.rb
@@ -5,5 +5,12 @@ RSpec.describe Planet, type: :model do
     planet = FactoryBot.create(:planet)
 
     expect(planet).to be_valid
-end
+  end
+
+  it 'is valid and appeared in 2 films' do
+    planet = FactoryBot.create(:planet, name: 'Alderaan')
+
+    expect(planet).to be_valid
+    expect(planet.number_of_appearances).to eq(2)
+  end
 end
